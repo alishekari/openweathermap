a66187defccb7e229f2473c97cdad53e62dd7ae8
Ali Shekari <alishekari1991@outlook.com>
2022-01-19 16:13:30 +0330

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+get weather json from openweathermap
+
+add this to your django settings:
+
+OPENWEATHERMAP_TOKEN = 'YOUR TOKEN'
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="persian-datetime",
+    version="0.1",
+    author="Ali Shekari",
+    author_email="alishekari1991@outlook.com",
+    description="openweathermap",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alishekari/python-persian-datetime",
+    packages=setuptools.find_packages(),
+    install_requires=['django', 'requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.0',
+)
